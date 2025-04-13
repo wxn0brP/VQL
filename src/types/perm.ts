0029567e6bb10906c8ba1e7609d508e448cc6702
wxn0brP <toja.filiper@gmail.com>
2025-04-13 13:53:22 +0200

    refactor: remove unused VQLPermissionCheck interface

--- src/types/perm.ts
+++ src/types/perm.ts
@@ -4,9 +4,3 @@ export enum PermCRUD {
     UPDATE = 1 << 2,
     DELETE = 1 << 3,
 }
-
-export interface VQLPermissionCheck {
-    type: "GateWarden" | "Internal" | "Custom";
-    flag?: number;
-    checkFn?: (user: any, data: any) => boolean | Promise<boolean>;
-}
