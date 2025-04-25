947785abdc7f3a264eee6ac26a6d3a92dea4e683
wxn0brP <toja.filiper@gmail.com>
2025-04-25 19:25:11 +0200

    feat: upgrade logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,12 @@
 import { ConsoleTransport, FileTransport, Logger } from "@wxn0brp/wts-logger";
 
-const logger = new Logger([
-    new ConsoleTransport(),
-    new FileTransport("data/execution.log")
-])
+const logger = new Logger({
+    transports: [
+        new ConsoleTransport(),
+        new FileTransport("data/execution.log")
+    ],
+    loggerName: "VQL"
+});
 
 export function logLog(instanceName: string, message: any) {
     const logMessage = `[${instanceName}] ${message}`;
