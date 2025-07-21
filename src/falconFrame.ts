221209e5e349d2b8f519a5d9fc5cd4fdfe8e6510
wxn0brP <toja.filiper@gmail.com>
2025-07-21 23:22:30 +0200

    feat: update FalconFrame integration

--- src/falconFrame.ts
+++ src/falconFrame.ts
@@ -1,3 +1,4 @@
+import { parseStringQuery } from "./cpu/string";
 import { type VQLProcessor } from "./processor";
 import type { FalconFrame, FFRequest } from "@wxn0brp/falcon-frame";
 
@@ -6,6 +7,7 @@ type ContextFn = (req: FFRequest) => Promise<any> | any;
 interface FF_VQL_Options {
     path?: string; // default = "/VQL"
     getUser?: ContextFn;
+    dev?: boolean
 }
 
 export function FF_VQL(app: FalconFrame, processor: VQLProcessor, options: FF_VQL_Options = {}) {
@@ -23,4 +25,15 @@ export function FF_VQL(app: FalconFrame, processor: VQLProcessor, options: FF_VQ
             return { err: true, msg: e.message };
         }
     });
+
+    if (options.dev) {
+        app.get(path+"-query", (req, res) => {
+            try {
+                return res.json(parseStringQuery(req.query?.query || ""));
+            } catch (e) {
+                res.status(500);
+                return { err: true, msg: e.message };
+            }
+        });
+    }
 }
