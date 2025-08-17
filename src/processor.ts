313e4bf27b90c980556e9a599e948498461e2856
wxn0brP <toja.filiper@gmail.com>
2025-08-17 16:28:35 +0200

    refactor: split VQLProcessor.execute

--- src/processor.ts
+++ src/processor.ts
@@ -4,7 +4,7 @@ import { executeRelation } from "./cpu/relation";
 import { executeQuery } from "./cpu/request";
 import logger from "./logger";
 import { replaceVars } from "./sheet";
-import { VQLError, VQLUQ } from "./types/vql";
+import { VQL_Query, VQLError, VQLUQ } from "./types/vql";
 import { validateRaw, validateVql } from "./valid";
 import { parseVQLS } from "./cpu/string";
 import { PermValidFn } from "./types/perm";
@@ -22,7 +22,38 @@ export class VQLProcessor {
         this.config = config instanceof VQLConfig ? config : new VQLConfig(config);
     }
 
-    async execute<T = any>(queryRaw: VQLUQ<T>, user: any = { _id: "null-null-null" }): Promise<T | VQLError> {
+    public async execute<T = any>(queryRaw: VQLUQ<T>, user: any = { _id: "null-null-null" }): Promise<T | VQLError> {
+        const result = this._preProcessQuery(queryRaw, user);
+        if ("err" in result) return result.err;
+
+        return await this._runQuery(result.query, user);
+    }
+
+    public _preProcessQuery(queryRaw: VQLUQ, user: any) {
+        const { query: parsedQuery, err: parseErr } = this._parseQuery(queryRaw);
+        if (parseErr) {
+            return { err: parseErr };
+        }
+
+        const validateRawResult = validateRaw(parsedQuery);
+        if (validateRawResult !== true) {
+            logger.warn("Raw validation failed:", validateRawResult);
+            return { err: validateRawResult };
+        }
+
+        const query = replaceVars(parsedQuery, user);
+        logger.debug("Executed sheet (expanded query):", query);
+
+        const validateVqlResult = validateVql(query);
+        if (validateVqlResult !== true) {
+            logger.warn("VQL validation failed:", validateVqlResult);
+            return { err: validateVqlResult };
+        }
+
+        return { query };
+    }
+
+    public _parseQuery(queryRaw: VQLUQ): { query?: VQL_Query; err?: VQLError } {
         if (typeof queryRaw === "string" || "query" in queryRaw) {
             logger.info("Incoming string query");
             const q = typeof queryRaw === "string" ? queryRaw : queryRaw.query;
@@ -34,7 +65,9 @@ export class VQLProcessor {
                 logger.debug("transformed query: ", queryRaw);
             } catch (e) {
                 logger.error("Error parsing string query: ", { error: e, msg: e.message });
-                return { err: true, c: 400, msg: `String query parsing error: ${e.message}` };
+                return {
+                    err: { err: true, c: 400, msg: `String query parsing error: ${e.message}` }
+                };
             }
 
             if (vars) queryRaw = { ...queryRaw, var: vars };
@@ -44,21 +77,10 @@ export class VQLProcessor {
             logger.debug("Raw query: ", queryRaw);
         }
 
-        const validateRawResult = validateRaw(queryRaw);
-        if (validateRawResult !== true) {
-            logger.warn("Raw validation failed:", validateRawResult);
-            return validateRawResult;
-        }
+        return { query: queryRaw }
+    }
 
-        const query = replaceVars(queryRaw, user);
-        logger.debug("Executed sheet (expanded query):", query);
-
-        const validateVqlResult = validateVql(query);
-        if (validateVqlResult !== true) {
-            logger.warn("VQL validation failed:", validateVqlResult);
-            return validateVqlResult;
-        }
-
+    public async _runQuery(query: VQL_Query, user: any) {
         if ("r" in query) {
             return await executeRelation(this, query, user);
         } else if ("d" in query) {
