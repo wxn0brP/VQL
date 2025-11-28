88d9cd7c4e173084bbd396b6777a34584fbce906
wxn0brP <toja.filiper@gmail.com>
2025-11-28 15:05:31 +0100

    feat(VQLS): prefix updateOneOrAdd & toggleOne support

--- src/cpu/string/utils.ts
+++ src/cpu/string/utils.ts
@@ -2,6 +2,8 @@ const opPrefix = {
     "-": "remove",
     "+": "add",
     "~": "update",
+    "?": "updateOneOrAdd",
+    "^": "toggleOne"
 }
 
 const operations = [
@@ -9,7 +11,7 @@ const operations = [
     "find", "findOne",
     "update", "updateOne",
     "remove", "removeOne",
-    "updateOneOrAdd",
+    "updateOneOrAdd", "toggleOne",
     "ensureCollection", "issetCollection",
 ];
 
@@ -54,7 +56,7 @@ export function extractMeta(input: string) {
     // Handle cases like "db update my.db.users s.name = 'John' u.name = 'Jane'"
     if (split.length > 3 && operations.includes(split[1])) {
         return { db: split[0], op: split[1], collection: split[2], body: split.slice(3).join(" ") };
-    } 
+    }
 
     // Default operation and initialization
     const db = split.shift();
@@ -64,7 +66,7 @@ export function extractMeta(input: string) {
 
     // Determine operation and collection based on special characters
     if (
-        ["!", "-", "+", "~"].some(c => split[0].includes(c)) || // Check if operation is indicated by special character
+        ["!", "-", "+", "~", "?", "^"].some(c => split[0].includes(c)) || // Check if operation is indicated by special character
         [".", ":", "{"].some(c => split[1].includes(c)) // Check if query body is indicated by special character
     ) {
         let temp = split.shift();
@@ -107,7 +109,7 @@ export function convertSearchObjToSearchArray(obj: Record<string, any>, parentKe
 
         if (!value) {
             return acc;
-        } else if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
+        } else if (typeof value === "object" && value !== null && !Array.isArray(value)) {
             return [...acc, ...convertSearchObjToSearchArray(value, currentPath)];
         } else {
             return [...acc, currentPath];
