54fc803c9b6a3a42c46357d791a13e202ebbb96d
wxn0brP <toja.filiper@gmail.com>
2025-12-18 08:20:26 +0100

    ci: fix build

--- rusalka/build.ts
+++ rusalka/build.ts
@@ -1,3 +1,3 @@
-export const postBuildCommands = `bun run types`;
+export const postBuildCommands = `bun remove @bun/types && bun run types`;
 export const publishToNpm = true;
 export const destDir = "types";
