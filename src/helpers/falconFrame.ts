5e0ea8d0f9191f1b9e1cf380af332519ad63c363
wxn0brP <toja.filiper@gmail.com>
2026-01-31 11:34:59 +0100

    feat: improve falcon frame integration

--- src/helpers/falconFrame.ts
+++ src/helpers/falconFrame.ts
@@ -1,38 +1,47 @@
 import { parseVQLS } from "../cpu/string";
 import { type VQLProcessor } from "../processor";
-import type { FalconFrame, FFRequest } from "@wxn0brp/falcon-frame";
+import type { FalconFrame, FFRequest, FFResponse } from "@wxn0brp/falcon-frame";
 
-type ContextFn = (req: FFRequest) => Promise<any> | any;
+export type ContextFn = (req: FFRequest, res: FFResponse) => Promise<any> | any;
 
 interface FF_VQL_Options {
-    path?: string; // default = "/VQL"
+    /** @default "/VQL" */
+    path?: string;
     getUser?: ContextFn;
-    dev?: boolean
+    dev?: boolean;
 }
 
-export function FF_VQL(app: FalconFrame, processor: VQLProcessor, options: FF_VQL_Options = {}) {
+function formatMessage(e: any) {
+    return e instanceof Error
+        ? e.message
+        : typeof e === "string"
+            ? e
+            : JSON.stringify(e);
+}
+
+export function FF_VQL(app: FalconFrame<any>, processor: VQLProcessor, options: FF_VQL_Options = {}) {
     const path = options.path || "/VQL";
     const getContext = options.getUser || (() => ({}));
 
     app.post(path, async (req, res) => {
         try {
-            const ctx = await getContext(req);
+            const ctx = await getContext(req, res);
             const result = await processor.execute(req.body.query, ctx);
             if (result && result.err) return result;
             return { err: false, result };
         } catch (e) {
             res.status(500);
-            return { err: true, msg: e.message };
+            return { err: true, msg: formatMessage(e) };
         }
     });
 
     if (options.dev) {
         app.get(path + "-query", (req, res) => {
             try {
-                return res.json(parseVQLS(req.query?.query || ""));
+                return parseVQLS(req.query?.query || "");
             } catch (e) {
                 res.status(500);
-                return { err: true, msg: e.message };
+                return { err: true, msg: formatMessage(e) };
             }
         });
     }
